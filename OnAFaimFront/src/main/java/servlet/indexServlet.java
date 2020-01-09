6ac55e51cfabdd98abe16f67e5ea0d894a3d3bac
unknown <thibautlesueur@hotmail.fr>
2020-01-09 14:15:52 +0100

    ajout des servlets

--- OnAFaimFront/src/main/java/servlet/indexServlet.java
+++ OnAFaimFront/src/main/java/servlet/indexServlet.java
@@ -1,21 +1,25 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.DAOPersonne;
+import dao.FactoryPersonne;
+import model.Personne;
+
+
 
 @WebServlet("/index")
 public class indexServlet extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
-		String maListe[] = {"Jacky", "Albert", "Michel", "Tom", "Dom", "Lola"};
-		request.setAttribute("listeMovie", maListe);
-		request.setAttribute("test", "jordan");
-		this.getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
+		this.getServletContext().getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
