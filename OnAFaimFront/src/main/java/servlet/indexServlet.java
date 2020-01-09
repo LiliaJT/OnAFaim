9df7e04308d2164c4e884d78fdb4b2f2f24e622f
lilia.dai@yahoo.fr <lilia.dai@yahoo.fr>
2020-01-09 16:15:29 +0100

    pom ok

--- OnAFaimFront/src/main/java/servlet/indexServlet.java
+++ OnAFaimFront/src/main/java/servlet/indexServlet.java
@@ -9,9 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import dao.DAOPersonne;
-import dao.FactoryPersonne;
-import model.Personne;
+
 
 
 
